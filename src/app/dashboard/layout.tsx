e8a717b4973e35e068c678a3041bc692c57cb190
Ndungu-Muigai <ndungu.muigai01@gmail.com>
2025-04-23 23:10:02 +0300

    Created and styled the sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,13 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/ui/app-sidebar";
- 
+import Navbar from "@/components/ui/Navbar";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => 
 {
     return ( 
         <SidebarProvider>
             <AppSidebar/>
-            <div className="flex flex-col h-screen w-full relative">
+            <div className="flex flex-col h-screen w-full">
                 <SidebarTrigger className="md:hidden"/>
                 {children}
             </div>
