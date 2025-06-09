d7dac1524aa6e968432498bc55f3aeac69a3cf7b
Ndungu-Muigai <ndungu.muigai01@gmail.com>
2025-06-09 17:04:27 +0300

    Edit and delete functionality completed

---
+++ src/components/MiniMap.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// components/MiniMap.tsx
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet"
+
+const MiniMap = ({ farm }: { farm: any }) => 
+{
+  const centerLat = farm?.geometry?.coordinates[0][0][1]
+  const centerLng = farm?.geometry?.coordinates[0][0][0]
+
+  console.log(farm?.geometry)
+  return (
+    <MapContainer style={{ height: "200px", width: "100%" }} center={[centerLat, centerLng]} zoom={35} scrollWheelZoom={true}>
+      <TileLayer attribution="&copy; OpenStreetMap contributors" url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+      <GeoJSON data={farm} style={() => ({color: "#228B22", weight: 2, fillColor: "#7CFC00", fillOpacity: 0.4})}/>
+    </MapContainer>
+  )
+}
+
+export default MiniMap
